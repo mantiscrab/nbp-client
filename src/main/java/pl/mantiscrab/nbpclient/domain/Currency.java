3597c4971f1ae4921d1ab261c0c4e76085915fab
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-08-20 22:34:04 +0200

    Currency unit test

--- src/main/java/pl/mantiscrab/nbpclient/domain/Currency.java
+++ src/main/java/pl/mantiscrab/nbpclient/domain/Currency.java
@@ -2,6 +2,7 @@ package pl.mantiscrab.nbpclient.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.EqualsAndHashCode;
+import pl.mantiscrab.nbpclient.domain.exceptions.CurrencyCreationException;
 
 @Embeddable
 @EqualsAndHashCode
@@ -17,17 +18,24 @@ public class Currency {
 
     public static Currency of(String currency) {
         if (currency == null)
-            throw new IllegalArgumentException();
+            throw new CurrencyCreationException("Currency name cannot be null");
         if (currency.length() != 3)
-            throw new IllegalArgumentException();
+            throw new CurrencyCreationException("Currency name must consist of exactly 3 letters");
         for (final char c : currency.toCharArray()) {
             if (!Character.isAlphabetic(c) || !Character.isUpperCase(c))
-                throw new IllegalArgumentException();
+                throw new CurrencyCreationException("Invalid currency name: " + currency);
         }
         return new Currency(currency);
     }
 
     public String asString() {
         return currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "currency='" + currency + '\'' +
+                '}';
     }
 }
