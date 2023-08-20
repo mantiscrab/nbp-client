3144912489cef4ecb9f1b67ec3420545591b4f6c
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-08-20 15:26:01 +0200

    Initial commit

---
+++ src/main/java/pl/mantiscrab/nbp/NbpClientApplication.java
@@ -0,0 +1,13 @@
+package pl.mantiscrab.nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbpClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mantiscrab/nbp/NbpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mantiscrab.nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
